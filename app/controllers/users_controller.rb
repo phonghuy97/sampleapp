62b2e725a164dd6aa643f1d42be165f6ad59cd0d
phonghuy97 <phonghuy97bg@gmail.com>
2019-06-27 08:48:49 +0700

    Chuong 8

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find_by_id params[:id]
+    return if @user
+    flash.now[:danger] = t("controllers.user.not_exits")
+    redirect_to root_path
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash.now[:success] = t("controllers.user.welcome")
+      redirect_to root_path
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit :name, :email, :password, :password_confirmation
+    end
+end
