2172764fe902f4b7138d0f88c6705b6ce6914c42
phonghuy97 <51810309+phonghuy97@users.noreply.github.com>
2019-06-27 14:50:31 +0700

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -5,14 +5,9 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 module SampleApp
   class Application < Rails::Application
-<<<<<<< HEAD
-	config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+  config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
 	config.i18n.available_locales = [:en, :vi]
   config.i18n.default_locale = :en
-
-
-=======
->>>>>>> 513b2a0023fbc987a34e5e5c081a98d3568da410
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
