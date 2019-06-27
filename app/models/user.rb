6dce6363e25c6d5d2499524288002c0a983aade7
phonghuy97 <phonghuy97bg@gmail.com>
2019-06-27 08:48:49 +0700

    Chuong 8

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  before_save {email.downcase!}
+  validates :name, presence: true, length: {maximum: Settings.maxName}
+  validates :email,presence: true, length: {maximum: Settings.maxMail},
+    format: {with: Settings.VALID_EMAIL_REGEX}
+  validates :password, presence: true, length: {minimum: Settings.minPass}
+
+  has_secure_password
+  class << self
+    def digest string
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+  BCrypt::Engine.cost
+      BCrypt::Password.create string, cost: cost
+    end
+  end
+end
--- config/initializers/new_framework_defaults.rb
+++ config/initializers/new_framework_defaults.rb
@@ -2,9 +2,7 @@
 #
 # This file contains migration options to ease your Rails 5.0 upgrade.
 #
-# Read the Guide for Upgrading Ruby on Rails for more info on each option.
-
-Rails.application.config.action_controller.raise_on_unfiltered_parameters = true
+# Read the Rails 5.0 release notes for more info on each option.
 
 # Enable per-form CSRF tokens. Previous versions had false.
 Rails.application.config.action_controller.per_form_csrf_tokens = true
@@ -18,9 +16,6 @@ ActiveSupport.to_time_preserves_timezone = true
 
 # Require `belongs_to` associations by default. Previous versions had false.
 Rails.application.config.active_record.belongs_to_required_by_default = true
-
-# Do not halt callback chains when a callback returns false. Previous versions had true.
-ActiveSupport.halt_callback_chains_on_return_false = false
 
 # Configure SSL options to enable HSTS with subdomains. Previous versions had false.
 Rails.application.config.ssl_options = { hsts: { subdomains: true } }
