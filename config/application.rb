f7594dbcf41bc0dfcf06de534097ed8d0aaca451
phonghuy97 <phonghuy97bg@gmail.com>
2019-06-27 11:15:59 +0700

    sua i18n

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(*Rails.groups)
 
 module SampleApp
   class Application < Rails::Application
+	config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+	config.i18n.available_locales = [:en, :vi]
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
