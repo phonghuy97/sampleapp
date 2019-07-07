f3fbe66380a760a22ce10863991802af86e0d950
Phong <phonghuy97bg@gmail.com>
2019-07-07 22:52:01 +0700

    add

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,11 @@ class User < ApplicationRecord
   validates :email,presence: true, length: {maximum: Settings.maxMail},
     format: {with: Settings.VALID_EMAIL_REGEX}
   validates :password, presence: true, length: {minimum: Settings.minPass}
-  
+
   has_secure_password
+  def digest string
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+  BCrypt::Engine.cost
+    BCrypt::Password.create string, cost: cost
+  end
 end
