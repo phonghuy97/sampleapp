91f61db70aaf36c552b83ebce28c86fdd02895d8
Phong <phonghuy97bg@gmail.com>
2019-07-07 21:08:03 +0700

    add

--- app/models/user.rb
+++
@@ -1,9 +0,0 @@
-class User < ApplicationRecord
-  before_save {email.downcase!}
-  validates :name, presence: true, length: {maximum: Settings.maxName}
-  validates :email,presence: true, length: {maximum: Settings.maxMail},
-    format: {with: Settings.VALID_EMAIL_REGEX}
-  validates :password, presence: true, length: {minimum: Settings.minPass}
-  
-  has_secure_password
-end
