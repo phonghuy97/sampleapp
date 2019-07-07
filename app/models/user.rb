45ba92c74d4bcb7ca1f8b301ac8958022340d699
phonghuy97 <51810309+phonghuy97@users.noreply.github.com>
2019-07-07 21:42:04 +0700

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   class << self
     def digest string
       cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-  BCrypt::Engine.cost
+      BCrypt::Engine.cost
       BCrypt::Password.create string, cost: cost
     end
   end
