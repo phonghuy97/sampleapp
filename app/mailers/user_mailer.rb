2f7aa1fe09e8f3806eac762e5f4dc621dfdc7c34
Phong <phonghuy97bg@gmail.com>
2019-07-09 11:12:54 +0700

    Chuong 11

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,11 @@
+class UserMailer < ApplicationMailer
+  def account_activation user
+    @user = user
+    mail to: user.email, subject: t("mailers.user_mailer.act_account")
+  end
+
+  def password_reset
+    @greeting = t(".hi")
+    mail to: user.email, subject: t("mailers.user_mailer.act_account")
+  end
+end
