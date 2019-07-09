c38663736a7b5caa580e8d76c2010df757b3539f
Phong <phonghuy97bg@gmail.com>
2019-07-09 11:52:43 +0700

    fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
-  before_action :correct_user,   only: [:edit, :update]
-  before_action :admin_user,     only: :destroy
+  before_action :logged_in_user, only: %i(index edit update destroy)
+  before_action :correct_user, only: %i(edit update)
+  before_action :admin_user, only: :destroy
   def new
     @user = User.new
   end
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.paginate(page: params[:page]).per(10)
   end
 
   def create
@@ -28,8 +28,6 @@ class UsersController < ApplicationController
   end
 
   def edit; end
-    @user = User.find_by_id params[:id]
-  end
 
   def update
     @user = User.find_by_id params[:id]
