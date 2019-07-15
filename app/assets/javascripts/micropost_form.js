0c17bdd7ebf7120ef0f4b0258653b3032b5c65b4
Phong <phonghuy97bg@gmail.com>
2019-07-15 11:48:41 +0700

    Chapter13

--- app/assets/javascripts/micropost_form.js
+++ app/assets/javascripts/micropost_form.js
@@ -1,10 +1,6 @@
 $('#micropost_picture').bind('change', function() {
   var size_in_megabytes = this.files[0].size/1024/1024;
   if (size_in_megabytes > 5) {
-<<<<<<< HEAD
     alert (I18n.t("model.micropost.notication"));
-=======
-    alert('Maximum file size is 5MB. Please choose a smaller file.');
->>>>>>> 39ef3c80ed52891d3b63ad445f8dbfb3eb1fdd5e
   }
 });
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_07_11_084945) do
 
   create_table "microposts", force: :cascade do |t|
