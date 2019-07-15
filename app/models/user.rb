775c9eb5c677b1dfc3e77995703e78a52423d302
phonghuy97 <51810309+phonghuy97@users.noreply.github.com>
2019-07-15 14:56:22 +0700

    Update app/models/user.rb
    Co-Authored-By: phucmh-1356 <49380498+phucmh-1356@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   attr_accessor :remember_token, :activation_token, :reset_token
   has_many :microposts, dependent: :destroy
-  has_many :active_relationships, class_name:  "Relationship",
+  has_many :active_relationships, class_name:  Relationship.name,
     foreign_key: "follower_id", dependent:   :destroy
   has_many :passive_relationships, class_name:  "Relationship",
     foreign_key: "followed_id", dependent:   :destroy
